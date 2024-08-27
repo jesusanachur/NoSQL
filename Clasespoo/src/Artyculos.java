bb309d8b6df09cef51c11d2a1f1b0f8876fb92b3
jesus <anachurycastro2001@gmail.com>
2024-08-26 19:10:49 -0500

    Clase_java

---
+++ Clasespoo/src/Artyculos.java
@@ -0,0 +1,15 @@
+
+public class Artyculos {
+    //Atributos
+    private double getPrecioventa
+
+    private String descripcion;
+    private double precioventa;
+    private int stock;
+       public Artyculos(String descripcion, int cantidad,double precio){
+           this.descripcion=descripcion;
+           precioventa=precio;
+           stock= cantidad;
+
+    }
+}
